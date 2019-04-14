e7a828d449e1da8c494fa12309d1466bc973b5d5
Rigo Rojas <rigo.rojas@gmail.com>
2019-04-14 12:56:41 -0700

    created marketplace layout

--- src/screens/marketplace/index.js
+++ src/screens/marketplace/index.js
@@ -1,22 +1,54 @@
-/**
- * This component is the main layout file which wraps the entire
- * application. You can call things here if you want them to load
- * when the application first loads.
- */
-
 import React from 'react';
-import styles from './marketplace.module.scss';
+import styles from './styles.js';
 import { subscribe } from 'react-contextual';
 
-
 const Marketplace = props => {
-
-
-  return (
-    <div className={styles.container}>
-      screens/marketplace
-    </div>
-  );
+    return (
+        <div style={styles.container}>
+            <div
+                className="subheader"
+                style={styles.subheader}
+            >
+                <h1>Marketplace</h1>
+            </div>
+            <div style={styles.layout}>
+                <div
+                    className="sidebar"
+                    style={styles.sidebar}
+                >
+                    <ul>
+                        <li>All Marketplace</li>
+                        <li>Home & Garden</li>
+                        <li>Housing</li>
+                        <li>Entertainment</li>
+                        <li>Clothing</li>
+                        <li>Family</li>
+                        <li>Electronics</li>
+                        <li>Hobies</li>
+                    </ul>
+                </div>
+                <div
+                    className="content"
+                    style={styles.content}
+                >
+                    <ul>
+                        <li>Post</li>
+                        <li>Post</li>
+                        <li>Post</li>
+                        <li>Post</li>
+                        <li>Post</li>
+                        <li>Post</li>
+                    </ul>
+                </div>
+            </div>
+            <div
+                className="subfooter"
+                style={styles.subfooter}
+            >
+                Subfooter
+            </div>
+        </div>
+    );
 }
 
 export default subscribe()(Marketplace);
---
+++ src/screens/marketplace/styles.js
@@ -0,0 +1,28 @@
+const styles = {
+    container: {
+        border: "10px solid red",
+    },
+    subheader:{
+        border: "10px solid purple",
+    },
+    title: {
+        color: "#636363",
+    },
+    layout: {
+        border: "10px solid green",
+        display: "flex",
+    },
+    sidebar: {
+        border: "10px solid pink",
+        width: "25%",
+    },
+    content: {
+        border: "10px solid orange",
+        width: "75%",
+    },
+    subfooter: {
+        border: "10px solid blue",
+    },
+}
+
+export default styles;
