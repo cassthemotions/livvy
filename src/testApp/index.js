2193a21fe2111cc429c01562a52ed4e96a420831
“Rawad Rifai” <rawad.rifai@gmail.com>
2019-04-27 16:34:25 -0700

    add user, get user working in test

--- src/testApp/index.js
+++ src/testApp/index.js
@@ -22,71 +22,31 @@ class TestApp extends Component {
 
   getAllUsers = async () => {
 
-    const response = await API.get('livvyAPI', '/users/all');
-    console.log(JSON.stringify(response, null, 2));
   }
 
   updateUser = async () => {
 
-    const body = {
-      firstName: "rawad",
-      lastName: "rifai"
-    };
 
-    let init = {
-      body: body
-    }
-    const response = await API.post('livvyAPI', '/users', init);
-    console.log(JSON.stringify(response, null, 2));
   }
 
 
   getMessage = async () => {
-    const id = "rawad;us-west-2:2abc67d6-1f53-4637-b5a7-dd671ee0e704;";
-    const timestamp = "1548404603473"
-    const response = await API.get('livvyAPI', '/messages/' + id + "/" + timestamp);
-    console.log(JSON.stringify(response, null, 2));
+
   }
 
   getMessagesWithDate = async () => {
 
-    let init = {
-      queryStringParameters: {
-        "FROM": 1548404603473
-      }
-    }
 
-    const id = "rawad;us-west-2:2abc67d6-1f53-4637-b5a7-dd671ee0e704;";
-    const response = await API.get('livvyAPI', '/messages/' + id, init);
-
-    console.log(JSON.stringify(response, null, 2));
   }
 
   putMessage = async () => {
 
-    let init = {
-      body: {
-        "to": ["rawad"],
-        "message": "hello"
-      }
-    }
-    const response = await API.put('livvyAPI', '/messages', init);
-    console.log(JSON.stringify(response, null, 2));
+
   }
 
   updateMessage = async () => {
 
-    const body = {
-      ETH: "999",
-      BTC: "456",
-      XRP: "123"
-    };
 
-    let init = {
-      body: body
-    }
-    const response = await API.post('livvyAPI', '/messages', init);
-    console.log(JSON.stringify(response, null, 2));
   }
 
 
