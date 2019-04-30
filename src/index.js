405abc0b54e0cf3afbaa705acca5a8de93916c73
Rigo Rojas <rigo.rojas@gmail.com>
2019-04-30 13:53:35 -0700

    Whitespace fix

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import { Route, Switch, Redirect } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-contextual';
-
 import Template from './ui/templates/livvy/';
-
 import Messenger from './ui/screens/messenger';
 import Marketplace from './ui/screens/marketplace';
 import Events from './ui/screens/events';
 import Maintenance from './ui/screens/maintenance';
 import Security from './ui/screens/security';
 import TestApp from './testApp';
-
 import store from './_shared/store';
 
 ReactDOM.render(
@@ -20,13 +17,35 @@ ReactDOM.render(
     <BrowserRouter>
       <Template>
         <Switch>
-          <Redirect exact path="/" to="/messenger" />
-          <Route path="/messenger" component={Messenger} />
-          <Route path="/marketplace" component={Marketplace} />
-          <Route path="/events" component={Events} />
-          <Route path="/maintenance" component={Maintenance} />
-          <Route path="/security" component={Security} />
-          <Route path="/test" component={TestApp} />
+            <Redirect
+                exact
+                path="/"
+                to="/messenger"
+            />
+            <Route
+                path="/messenger"
+                component={Messenger}
+            />
+            <Route
+                path="/marketplace"
+                component={Marketplace}
+            />
+            <Route
+                path="/events"
+                component={Events}
+            />
+            <Route
+                path="/maintenance"
+                component={Maintenance}
+            />
+            <Route
+                path="/security"
+                component={Security}
+            />
+            <Route
+                path="/test"
+                component={TestApp}
+            />
         </Switch>
       </Template>
     </BrowserRouter>
